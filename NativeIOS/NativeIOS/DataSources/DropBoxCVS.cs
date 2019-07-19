8035f53057ddb0ade378f7cfa00f82869b66b8d8
Srinivasu Katta <srinivasu0330@gmail.com>
2019-07-19 16:54:42 +0530

    Changes

---
+++ NativeIOS/NativeIOS/DataSources/DropBoxCVS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using NativeIOS.Model;
+using UIKit;
+
+namespace NativeIOS
+{
+    public class DropBoxCVS : UICollectionViewSource
+    {
+        private List<DropBoxData.DropBoxRow> data;
+
+        public DropBoxCVS(List<DropBoxData.DropBoxRow> data)
+        {
+            this.data = data;
+        }
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, Foundation.NSIndexPath indexPath)
+        {
+            var cell = (CVCell)collectionView.DequeueReusableCell("Cellid", indexPath);
+
+            var dropBoxContent = data[indexPath.Row];
+
+            cell.UpdateDropBoxData(dropBoxContent);
+
+            return cell;
+        }
+       
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            return data.Count;
+        }
+    }
+}
